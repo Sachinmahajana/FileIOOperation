cbe6e2c28e03d9590050eecb3a7ac14c7b1426bc
Sachinmahajana <sachinmahajan782@gmail.com>
2023-05-25 20:20:49 +0530

    [Sachin] Adding JsonSerialization

---
+++ FileIOOperation/JsonDataOperations.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOperation
+{
+    internal class JsonDataOperations
+    {
+        //Json DataSerialization 
+        public static void JsonDataSerialize()
+        {
+            string jsonpath = "D:\\CsharpBasicprograms\\FileIOOperation\\FileIOOperation\\FileIO\\JsonData.JSON";
+
+            try
+            {
+                List<Contact> contact = new List<Contact>()
+                {
+               new Contact() {Name = "Ajay",Id = 295,Age = 23},
+               new Contact() {Name = "Surend",Id=293,Age = 24},
+               new Contact() {Name = "Sandip",Id = 287,Age= 22 }
+
+                };
+                string data = JsonConvert.SerializeObject(contact);
+                File.WriteAllText(jsonpath, data);
+                Console.WriteLine("Json Serialization Completed Sucessfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        } 
+        //Json DataDeserialization
+        public static void JsonDataDeserialize()
+        {
+            string jsonpath = "D:\\CsharpBasicprograms\\FileIOOperation\\FileIOOperation\\FileIO\\JsonData.JSON";
+            try
+            {
+                string jsondata = File.ReadAllText(jsonpath);
+                List<Contact> list = JsonConvert.DeserializeObject<List<Contact>>(jsondata);
+
+                foreach (Contact contacts in list)
+                {
+                    Console.WriteLine(contacts);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
